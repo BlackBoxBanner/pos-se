26dfe1b8dd8e1ab029cc658807fc52c4c9a5cdb9
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-05 21:23:48 +0700

    add address api

---
+++ src/app/api/address/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { createAddress, CreateAddressProps, deleteAddress, DeleteAddressProps } from "@/utils/address"
+
+export async function POST(request: Request) {
+  // get body out of request data
+  const { data, userId } = (await request.json()) as CreateAddressProps
+
+  try {
+    return NextResponse.json(await createAddress({ userId, data }))
+  } catch (error) {
+    if (error instanceof Error) NextResponse.json(error.message, {
+      status: 400
+    })
+  }
+
+  return NextResponse.json("")
+}
+
+export async function DELETE(request: Request) {
+  // get body out of request data
+  const { id } = (await request.json()) as DeleteAddressProps
+
+  try {
+    return NextResponse.json(await deleteAddress({ id }))
+  } catch (error) {
+    if (error instanceof Error) NextResponse.json(error.message, {
+      status: 400
+    })
+  }
+
+  return NextResponse.json("")
+}
