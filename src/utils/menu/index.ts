f3693bff8afbcac06ab98860eb5b4e209b75e1d5
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-08 00:12:09 +0700

    create add menu

--- src/utils/menu/index.ts
+++ src/utils/menu/index.ts
@@ -1,4 +1,4 @@
-import { Menu } from '@prisma/client'
+import { $Enums, Menu } from '@prisma/client'
 import prisma from '@/utils/prisma'
 
 export type GetFilterNameMenuProps = {
@@ -26,4 +26,33 @@ export type SearchMenuProps = {
 type SearchMenus = (props: SearchMenuProps) => Menu[]
 export const searchMenus: SearchMenus = ({ menu, name }) => {
 	return menu.filter((item) => item.name.includes(name))
+}
+
+export type CreateMenuProps = Pick<
+	Menu,
+	'name' | 'image' | 'price' | 'status' | 'type'
+>
+type CreateMenu = (props: CreateMenuProps) => Promise<Menu>
+export const createMenu: CreateMenu = async ({
+	name,
+	type,
+	image,
+	price,
+	status,
+}) => {
+	if (!name) throw new Error('No name provided.')
+	if (!type) throw new Error('No type provided.')
+	if (!image) throw new Error('No image provided.')
+	if (!price) throw new Error('No price provided.')
+	if (!status) throw new Error('No status provided.')
+
+	return prisma.menu.create({
+		data: {
+			name,
+			type,
+			image,
+			price,
+			status,
+		},
+	})
 }
