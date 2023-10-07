dd991fc2552d9ca9f4a816f3884e57a67df60f55
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-07 20:44:30 +0700

    create get Menu api

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -1,5 +1,16 @@
 import { NextResponse, NextRequest } from 'next/server'
+import { GetFilterNameMenuProps, getMenu } from '@/utils/menu'
 
 export async function GET(request: NextRequest) {
-	return NextResponse.json('')
+	const { searchParams } = new URL(request.url)
+	const name = searchParams.get('name') as GetFilterNameMenuProps['name']
+
+	try {
+		return NextResponse.json(await getMenu({ name }))
+	} catch (error: unknown) {
+		if (error instanceof Error)
+			return NextResponse.json(error.message, {
+				status: 400,
+			})
+	}
 }
