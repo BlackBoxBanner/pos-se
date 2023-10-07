1f938c442a8774d8c4bfef6e2b8c90176f5341f4
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-07 17:02:52 +0700

    create getMenus function

--- src/utils/menu/index.ts
+++ src/utils/menu/index.ts
@@ -1,4 +1,8 @@
-type GetMenus = () => Promise<unknown>
+import { Menu } from '@prisma/client'
+import prisma from '@/utils/prisma'
+
+type GetMenus = () => Promise<Menu[]>
 export const getMenus: GetMenus = async () => {
-	return
+	return prisma.menu.findMany({})
 }
+
