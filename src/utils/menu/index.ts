27bbae5824eb761b05bcc14737ea0016b3e3aef4
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-08 11:35:55 +0700

    add space and format the code

--- src/utils/menu/index.ts
+++ src/utils/menu/index.ts
@@ -28,14 +28,12 @@ export const searchMenus: SearchMenus = ({ menu, name }) => {
 	return menu.filter((item) => item.name.includes(name))
 }
 
-
 export type DeleteMenuProps = {
 	id: string
 }
 
 type DeleteMenu = (props: DeleteMenuProps) => Promise<Menu>
 export const deleteMenu: DeleteMenu = async ({ id }) => {
-
 	if (!id) throw new Error('No id provided')
 
 	return prisma.menu.delete({
