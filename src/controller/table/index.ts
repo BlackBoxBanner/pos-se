286b22c96ad792309f709fcdd226a42e8641a38f
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-10-13 15:38:11 +0700

    feat: change filelocation

---
+++ src/controller/table/index.ts
@@ -0,0 +1,24 @@
+import { Table } from 'prisma/prisma-client'
+import prisma from '@/utils/prisma'
+
+export type CreateTableProps = Omit<Table, 'id' | 'createAt' | 'updatedAt'>
+type CreateTable = (props: CreateTableProps) => Promise<Table>
+
+export const createTable: CreateTable = async ({ tableNumber, seat }) => {
+	if (!tableNumber) throw new Error('No tableNumber provided.')
+	if (!seat) throw new Error('No seat provided.')
+
+	// check a table to prevent duplicate table
+	const existingTable = await prisma.table.findUnique({
+		where: { tableNumber },
+	})
+
+	if (!!existingTable) throw new Error('Already have this tableNumber.')
+
+	return prisma.table.create({
+		data: {
+			tableNumber,
+			seat,
+		},
+	})
+}
